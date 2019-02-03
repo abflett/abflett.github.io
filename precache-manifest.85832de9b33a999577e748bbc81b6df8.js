652232d9b974970e9a2e4c2f370dcb40df361313
abflett <abflett@yahoo.ca>
2019-02-03 12:17:56 -0600

    new webpage

---
+++ precache-manifest.85832de9b33a999577e748bbc81b6df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf97c83eb3afef5cb935b4a94cfbad2c",
+    "url": "/static/media/adam.cf97c83e.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b9d8d1ae7129a48619bc",
+    "url": "/static/js/main.b9d8d1ae.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "b9d8d1ae7129a48619bc",
+    "url": "/static/css/main.e78271ec.chunk.css"
+  },
+  {
+    "revision": "ff76d4119a179da0d9cff09146995cc6",
+    "url": "/index.html"
+  }
+];
